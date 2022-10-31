7a8b48dedce708f09ce161a3302858dfde76742b
KLChakraborty <chakrabortykajal2@gmail.com>
2022-10-31 21:35:30 +0530

    modified

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,14 +1,26 @@
 const bookModel = require("../models/bookModel")
 
+const mongoose = require("mongoose")
+const objectId = mongoose.isValidObjectId
+
 const createBook = async function (req, res) {
     const { author } = req.body
     if (!author) {
         return res.send("author detail is required")
     }
+    
     const { publisher } = req.body
     if (!publisher) {
         return res.send("publisher detail is required")
     }
+    
+    if(!objectId(author)){
+        return res.send("enter valid author id")
+    } 
+    if(!objectId(publisher)){
+        return res.send("enter valid publisher id")
+    }
+
     const savedData2 = await bookModel.create(req.body)
     return res.send({ msg: savedData2 })
 }
